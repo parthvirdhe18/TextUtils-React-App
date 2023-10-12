3e6f306afc9bcbdedcc9e326e0b407dc7e56472a
Parth Virdhe <119056314+parthvirdhe18@users.noreply.github.com>
2023-10-12 19:11:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/textForm';
+import About from './components/About';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
+ 
+// import {BrowserRouter ,Routes, Link , Route } from "react-router-dom";
+
 
 function App() {
+
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled orr not
+  const [alert,setAlert]= useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor='#042743'
+    showAlert("Dark mode has been enabled", "success")
+    document.title="TextUtils - Dark Mode"
+
+    setInterval(() => {
+      document.title="TextUtils is Amazing!!!"
+    }, 2000);
+    setInterval(() => {
+      document.title="Install TextUtil Now!"
+    }, 1500);
+
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert("Light mode has been enabled", "success")
+      document.title="TextUtils - Light Mode"
+    } 
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <TextForm heading="Enter the text to Analyze Below" showAlert={showAlert} mode={mode}/>
+     {/* <About/> */}
+
+<div className="container my-3">
+
+ 
+</div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Navbar from './components/Navbar';
+// import TextForm from './components/textForm';
+import About from './components/About';
+import Alert from './components/Alert';
+
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export default function AppF() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={< App/>}/>
+        <Route axact path="/" element={< Alert/>}/>
+        <Route exact path="/about" element={< About/>}/>
+        
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <AppF />
   </React.StrictMode>
 );
 
